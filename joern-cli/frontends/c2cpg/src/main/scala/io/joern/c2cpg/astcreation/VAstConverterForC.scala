46cbd8ef2d8ce37c59734f79f8e9dff847a62727
Fynn <uwquu@student.kit.edu>
2026-05-31 15:02:51 +0200

    Add VAstConverterForC.

---
+++ joern-cli/frontends/c2cpg/src/main/scala/io/joern/c2cpg/astcreation/VAstConverterForC.scala
@@ -0,0 +1,13 @@
+package io.joern.c2cpg.astcreation
+
+import io.joern.c2cpg.astcreation.converter.{VAstConverter, VAstPatternConverter, VAstPatternConverterForBinaryOperators, VAstPatternConverterForConditionalMacro, VAstPatternConverterForFunctionDeclaration, VAstPatternConverterForSuperCRoot, VAstPatternConverterForVariableDeclaration}
+
+class VAstConverterForC(private var vAstCreator: VAstCreatorNew) extends VAstConverter {
+  private val patterns: List[VAstPatternConverter] = List.apply(
+    new VAstPatternConverterForBinaryOperators(vAstCreator, this),
+    new VAstPatternConverterForConditionalMacro(vAstCreator, this),
+    new VAstPatternConverterForSuperCRoot(vAstCreator, this),
+    new VAstPatternConverterForVariableDeclaration(vAstCreator, this)
+  )
+  super.addPatterns(patterns)
+}
